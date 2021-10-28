c9403a84c37eec905887239c5c6823728e6be150
Guillaume Lamanda <guillaume.lamanda@gmail.com>
2021-10-28 15:45:31 +0200

    test: ignore data folder in watch mode

--- jest.config.mjs
+++ jest.config.mjs
@@ -187,7 +187,7 @@ export default {
   // verbose: undefined,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  // watchPathIgnorePatterns: [],
+  watchPathIgnorePatterns: ["data", "tests/data"],
 
   // Whether to use watchman for file crawling
   // watchman: true,
